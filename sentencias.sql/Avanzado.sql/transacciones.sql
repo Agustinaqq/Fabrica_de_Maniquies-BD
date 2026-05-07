df1a1a1059b9451f6bf79e88ff8eb9b0e4bab076
Agustinaqq <agustinaqq@gmail.com>
2026-05-07 19:50:08 -0300

    Modificaciones

--- sentencias.sql/Avanzado.sql/transacciones.sql
+++ sentencias.sql/Avanzado.sql/transacciones.sql
@@ -2,13 +2,8 @@ USE fabrica_maniquies;
 
 START TRANSACTION;
 
--- Creamos un nuevo modelo de maniquí
-INSERT INTO maniquies (nombre_modelo, estado) 
-VALUES ('Modelo Prototipo X1', 'Disponible');
-
--- Le asignamos su primera pieza usando el último ID generado
+INSERT INTO maniquies (nombre_modelo, estado) VALUES ('Modelo Prototipo X1', 'Disponible');
 INSERT INTO piezas (tipo_pieza_id, material_id, color_id, id_maniqui, talle, fecha_fabricacion)
 VALUES (1, 1, 1, LAST_INSERT_ID(), 'Grande', CURDATE());
 
--- Si ambos INSERT funcionan, confirmamos
 COMMIT;
